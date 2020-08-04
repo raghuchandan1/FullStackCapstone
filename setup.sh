88f92539236b6d878556d4dfef29d2465d542346
Raghu Chandan <33779280+Raghu-Chandan@users.noreply.github.com>
2020-08-04 14:21:58 +0530

    Optimized imports

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 export AUTH0_DOMAIN="raghuchandan1.us.auth0.com"
 export API_AUDIENCE="https://localhost:5000"
-export ALGORITHMS='["RS256"]'
+export ALGORITHMS=""['RS256']""
 export DATABASE_URL="postgres://ipyofrolkuockm:f01eefdf6cf82a0d27355e8421545cc2776a726c8d8a513f488b5ab60fe33ab6@ec2-52-204-232-46.compute-1.amazonaws.com:5432/dbmq281fou3bs9"
